349061ee4edda5af4703b3714c239a389673531a
SheikhIshere <thesheikh255@gmail.com>
2025-10-02 21:19:07 +0600

    ch6 done permission system

--- Blog-api/backend/posts/views.py
+++ Blog-api/backend/posts/views.py
@@ -3,13 +3,22 @@ from rest_framework.generics import (
     ListCreateAPIView, 
     RetrieveUpdateDestroyAPIView,
 )
+
+from rest_framework.permissions import (
+    IsAuthenticated,
+)
+
+from .permissions import IsAuthorOrReadOnly
+
 from .models import Post
 from .serializers import PostSerializer
 
 class PostList(ListCreateAPIView):
+    """ permission_classes = [IsAuthenticated] """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthorOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
